355be473682011090efb2c5a08485795a360fefa
Caleb Grimah <lamahgrimah@gmail.com>
2024-04-13 10:56:06 +0100

    add poppins font files, expo splash screen

--- App.tsx
+++ App.tsx
@@ -1,7 +1,27 @@
+import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
+import { useCallback } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import * as SplashScreen from 'expo-splash-screen';
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    'Poppins-Bold': require('./assets/fonts/Poppins-Bold.ttf'),
+    'Poppins-Medium': require('./assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-Light': require('./assets/fonts/Poppins-Light.ttf'),
+    'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf'),
+  });
+  
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
